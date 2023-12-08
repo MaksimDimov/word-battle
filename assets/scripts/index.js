9c0375a143e8204353aedb9b768e713fdec3332c
MaksimDimov <maksymdimov@student.mitt.ca>
2023-12-08 07:46:05 +0200

    fixed problems and responsive(maybe)

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,52 +1,10 @@
 'use strict';
 
-import  Score  from './classes.js';
+import  Score  from './Score.js';
 
-function onEvent(event, selector, callback) {
-    return selector.addEventListener(event, callback);
-  }
-  
-  // Get HTML element by id
-  function getElement(selector, parent = document) {
-    return parent.getElementById(selector);
-  }
-  
-  // Select HTML element
-  function select(selector, parent = document) {
-    return parent.querySelector(selector);
-  }
-  
-  // Get a (node) list of HTML elements as array
-  function selectAll(selector, parent = document) {
-    return [...parent.querySelectorAll(selector)];
-  }
-  
-  // Print
-  function print(arg) {
-    console.log(arg);
-  }
-  
-  // Sleep
-  function sleep(duration) {
-    return new Promise(resolve => {
-      setTimeout(resolve, duration)
-    });
-  }
-  
-  // Generate random number between - and including - 'min' and 'max'
-  function randomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  }
-  
-  // Filter array
-  function filterArray(array, callback) {
-    return array.filter(callback);
-  }
-  
-  // Create an HTML element
-  function create(element, parent = document) {
-    return parent.createElement(element);
-  }
+import {
+  onEvent,  getElement,  select,  selectAll,  print,  sleep,  randomNumber,  filterArray,  create
+} from './Utilities.js';
 
   const timerNumbers = select('.timer-numbers');
   const score = select('.score');
@@ -97,10 +55,10 @@ const words = [
         let currentTime = new Date().getTime();
         let remainingTime = endTime - currentTime;
         hits.textContent = `Score: ${scoreValue}`;
-        date.textContent = `Date: ${new Date().toLocaleDateString()}`;
-        persentage.textContent = `Percentage: ${Math.round((scoreValue / words.length) * 100)}%`;
-        let finalScoreInfo = new Score(hits.textContent, date.textContent, persentage.textContent);
-        print(finalScoreInfo.getInfo());
+        // date.textContent = `Date: ${new Date().toLocaleDateString()}`;
+        // persentage.textContent = `Percentage: ${Math.round((scoreValue / words.length) * 100)}%`;
+        // let finalScoreInfo = new Score(hits.textContent);
+        // print(finalScoreInfo.getInfo());
 
         startButton.textContent = 'Restart';
     
@@ -125,20 +83,20 @@ const words = [
         let timerInterval = setInterval(updateTimer, 1000);
 }
 
-
     function startGame() {
+      backgroundMusic.currentTime = 0;
+      if (backgroundMusic.paused) {
         backgroundMusic.play();
+      }
         gameActive = true;
         scoreValue = 0;
-        hits.textContent = 'Score: 0';
-        persentage.textContent = 'Percentage: 0%';
+        // hits.textContent = 'Score: 0';
+        // persentage.textContent = 'Percentage: 0%';
         timerDiv.classList.remove('hidden');
         gameDiv.classList.remove('hidden');
         startTimer(99);
         changeWord();
     }
-
-
 
     function changeWord() {
         let randomWord = words[Math.floor(Math.random() * words.length)];
